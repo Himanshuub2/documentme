eeee5ae4e1bd8f60cf97fed411823f4801a1f4ef
himanshuub2 <2121997him@gmail.com>
2024-05-08 10:41:14 +0530

    adds multiple images support with openai and adds concurrently

--- server/services/documentService.ts
+++ server/services/documentService.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
-import getScreenShots from "./screenshotService";
-import { imagetoText } from "./imagetoText";
+import getScreenShots from "../utils/screenshotService";
+import { imagetoText } from "../utils/imagetoText";
 
 
 export const documentService = async (req:Request,res:Response) => {
@@ -8,7 +8,7 @@ export const documentService = async (req:Request,res:Response) => {
     const imageArray = await getScreenShots(url,endpoints);
     const prompt = 'write simple document from above image its written in react'
 
-    const response = await imagetoText(prompt , imageArray[0]);
-
+    const imageText = await imagetoText(prompt , imageArray);
+    console.log(imageText.choices[0].message)
     res.status(200).json({message:"successfull",images:imageArray})
 }
